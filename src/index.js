59a82dcc86dd03be5c745f6dd3c1135ec0f55a08
Juanoso <jcamilo994@hotmail.com>
2022-02-22 09:43:33 -0500

    3.2

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar } from './components/Navbar';
-import Home from './container/Home';
-//import AppRouter from './routers/AppRouter';
+import AppRouter from './routers/AppRouter';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <AppRouter/> */}
-    <Navbar/>
-    <Home/>
+    <AppRouter/>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
